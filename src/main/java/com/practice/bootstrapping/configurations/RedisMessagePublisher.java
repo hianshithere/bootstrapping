23c21b3c0c1fe67de6a7155a83e277cee815b4a3
Anshit Kumar <anshitkumarsharma@yahoo.in>
2022-12-06 09:40:02 +0530

    Working redis implementation

---
+++ src/main/java/com/practice/bootstrapping/configurations/RedisMessagePublisher.java
@@ -0,0 +1,27 @@
+package com.practice.bootstrapping.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisMessagePublisher implements MessagePublisher {
+
+    @Autowired
+    public RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public ChannelTopic topic;
+
+    public RedisMessagePublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
+        this.redisTemplate = redisTemplate;
+        this.topic = topic;
+    }
+
+    @Override
+    public void publish(String message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
